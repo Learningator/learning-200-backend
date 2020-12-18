5d6f172be9b1800621607b4af512475aa3fb4088
Joel Gaspar <joel.e.gaspar@gmail.com>
2020-12-18 10:34:16 -0700

    writing base of itneraction with ds api

--- src/services/mainService.js
+++ src/services/mainService.js
@@ -1,8 +1,23 @@
 'use strict'
+const axios = require('axios')
 
 class MainServices {
   constructor() {}
-
+  getCompletedCourses = async (user) =>{
+    let courses;
+    const url;
+    try{
+      courses = await axios.get(url, {
+        params: {
+          user: user
+        }
+      })
+      return courses || {}
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
 
 }
 
