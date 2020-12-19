6e00fb204c382e746ff0335bc7c1edbea0d543fc
Joel Gaspar <joel.e.gaspar@gmail.com>
2020-12-19 11:41:03 -0700

    deleting comments in code

--- src/services/mainService.js
+++ src/services/mainService.js
@@ -1,16 +1,6 @@
 'use strict'
 const axios = require('axios');
 
-const fake_lp = {
-  'LearningPath':
-    [
-      {
-        'course1': 'Ingenieria de datos conn python', 
-        'course2': 'Curso de Algo'
-      }
-    ]
-}
-
 
 class MainServices {
   constructor() {}
@@ -20,44 +10,23 @@ class MainServices {
     const url = 'https://learning-200-scraper.herokuapp.com/api/' + user;
     try{ 
       courses = await axios.get(url) 
-      //console.log(courses.data)
       return courses.data || {}
     }
     catch(err){
       console.error(err)
     }
   }
-  getCutomizedLP = async(response) => {
-    const url = 'www'
-    try{
-      /*
-      const lp = await axios.get(url, response)
-      if (lp){
-        return lp
-      }
-      else{
-        return {}
-      } */
-      console.log(response)
-      return fake_lp
-    }catch(err){
-      console.error(err)
-    }
-
-  }
+ 
 
   sendInfoToML = async ({params}) => {
-    const url = ''
-    let customizedLP;
     try{
       let response = await this.getCompletedCourses(params.user)
       if (response){
         response['field'] = params.field
         response['subfield'] = params.subfield
         response['user'] = params.user
-        customizedLP = await this.getCutomizedLP(response)
 
-        return customizedLP || {}
+        return response || {}
       }
       else{
         return {}
